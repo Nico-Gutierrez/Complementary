d45557e4fc302f0e2fa4d95ec58994f81d46cf10
Viviana Luna <90412539+CatSnoopy@users.noreply.github.com>
2024-07-26 15:27:58 -0500

    Delete app directory

--- app/Business/UserBusiness.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Business;
-
-/*contiene la lógica de negocio de la aplicación*/
-
-class UserBusiness{
-
-}
--- app/DAO/UserDAO.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\DAO;
-/*Interactua con la bases de Datos directamente*/
-
-class UserDAO{
-
-}
