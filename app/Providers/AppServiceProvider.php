d57eb7f134c95183aa29315425e87864ae336d09
Viviana Luna <90412539+CatSnoopy@users.noreply.github.com>
2024-07-30 09:54:16 -0500

    Add files via upload

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Business\UserBusiness;
+use App\DAO\UserDAO;
+use App\Services\UserServices;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,13 +14,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(UserDAO::class, function ($app) {
+            return new UserDAO();
+        });
+
+        $this->app->singleton(UserBusiness::class, function ($app) {
+            return new UserBusiness($app->make(UserDAO::class));
+        });
+
+        $this->app->singleton(UserServices::class, function ($app) {
+            return new UserServices($app->make(UserBusiness::class));
+    });
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
         //
     }
